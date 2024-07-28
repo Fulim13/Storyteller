be2e40700faa0393d00c8e3a8d30783ba310530f
Fu Lim <fulim1130@gmail.com>
2024-07-28 23:48:44 +0800

    update for ollma3:7b

--- get_llm.py
+++ get_llm.py
@@ -1,4 +1,6 @@
 from langchain_openai import OpenAI
+from langchain_ollama.llms import OllamaLLM
+
 from dotenv import load_dotenv
 import openai
 import os
@@ -8,5 +10,6 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 def get_llm():
-    llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0)
+    # llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0)
+    llm = OllamaLLM(model="llama3")
     return llm
